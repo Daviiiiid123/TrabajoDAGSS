0e1de23aff94150a0a0f0ccf7a2de5e7d2a33da6
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2025-01-05 12:54:49 +0100

    Administrador, CentroSalud, Farmacia, Medicamento

--- src/main/java/es/uvigo/dagss/recetas/entidades/Administrador.java
+++ src/main/java/es/uvigo/dagss/recetas/entidades/Administrador.java
@@ -1,17 +1,45 @@
 package es.uvigo.dagss.recetas.entidades;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.TableGenerator;
 
 @Entity
 @DiscriminatorValue(value = "ADMINISTRADOR")
 public class Administrador extends Usuario {
 
-    // Anadir atributos propios
+    @Id
+    @TableGenerator(name = "ADMINISTRADOR_GEN", table = "GENERATOR_TABLE", pkColumnName = "GEN_KEY", valueColumnName = "GEN_VALUE", pkColumnValue = "ADMINISTRADOR_ID", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "ADMINISTRADOR_GEN")
+    private Long id;
 
+    @Column(name="NOMBRE", length = 50, nullable = false)
     private String nombre;
 
+    @Column(name="EMAIL", length = 50, nullable = false)
     private String email;
+
+    private String login;
+    private String password;
+
+    private Boolean activo = true;
+
+    //(1,N) Administrador gestiona (1,N) CentroSalud
+    @ManyToMany
+    @JoinTable(name = "ADMINISTRADOR_CENTROSALUD",
+            joinColumns = @JoinColumn(name = "ADMINISTRADOR_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CENTROSALUD_ID"))
+    private List<CentroSalud> centrosSalud;
+
 	
     public Administrador() {
         super(TipoUsuario.ADMINISTRADOR); 
@@ -50,5 +78,54 @@ public class Administrador extends Usuario {
         }
         return valido;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + ", login=" + login + ", password=" + password + ", activo=" + activo + '}';
+    }
+    
+    //Metodo hashCode
+    @Override
+    public int hashCode() {
+        if (this.id != null) {
+            return this.id.hashCode();
+        }
+        return super.hashCode();
+    }
+
+    //Metodo equals
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Administrador other = (Administrador) obj;
+        if (this.id != null) {
+            return this.id.equals(other.getId());
+        }
+        return super.equals(obj);
+    }
+    
 
 }
--- src/main/java/es/uvigo/dagss/recetas/entidades/CentroSalud.java
+++ src/main/java/es/uvigo/dagss/recetas/entidades/CentroSalud.java
@@ -1,37 +1,71 @@
 package es.uvigo.dagss.recetas.entidades;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.TableGenerator;
 
 @Entity
 public class CentroSalud {
 
     @Id
-    @TableGenerator(name = "USUARIO_GEN", table = "USUARIO_GEN", pkColumnName = "GEN_NAME", valueColumnName = "GEN_VAL", allocationSize = 1)           
+    @TableGenerator(name = "USUARIO_GEN", table = "USUARIO_GEN", pkColumnName = "GEN_KEY", valueColumnName = "GEN_VAL", allocationSize = 1)           
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "USUARIO_GEN")
     private Long id;
 
+    @Column(name = "NOMBRE", length = 100, nullable = false)
     private String nombre;
+
+    @Column(name = "DIRECCION", length = 200, nullable = false)
     private String direccion;
+
+    @Column(name = "TELEFONO", length = 15, nullable = false)
     private String telefono;
+
+    @Column(name = "ACTIVO", nullable = false)
     private boolean activo;
+
+    @Column(name = "EMAIL", length = 100, nullable = false)
     private String email;
 
-    @ManyToOne(targetEntity = Administrador.class)
-    private Administrador administrador;
+    //(1,N) CentroSalud es gestionado por (1,N) Administrador
+    @ManyToMany(targetEntity = Administrador.class)
+    @JoinTable(name = "ADMINISTRADOR_CENTROSALUD",
+            joinColumns = @JoinColumn(name = "CENTROSALUD_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ADMINISTRADOR_ID"))   
+    private List<Administrador> administradores;
+
+    //(1,N) CentroSalud tiene (1,1) Medico
+    @ManyToOne(targetEntity = Medico.class)
+    @JoinColumn(name = "MEDICO_ID")
+    private Medico medico;
+
+    //(1,1) CentroSalud tiene (1,N) Paciente
+    @OneToMany(targetEntity = Paciente.class)
+    @JoinColumn(name = "CENTROSALUD_ID")
+    private List<Paciente> pacientes;
+    
+
 
 
     public CentroSalud() {
     }
 
-    public CentroSalud(String nombre, String direccion, String telefono) {
+    public CentroSalud(String nombre, String direccion, String telefono, boolean activo, String email) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
+        this.activo = activo;
+        this.email = email;
     }
 
     public String getNombre() {
@@ -99,5 +133,45 @@ public class CentroSalud {
         }
         return valido;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "CentroSalud{" + "id=" + id + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", activo=" + activo + ", email=" + email + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        if (this.id != null) {
+            return this.id.hashCode();
+        }
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        CentroSalud other = (CentroSalud) obj;
+        if (this.id != null) {
+            return this.id.equals(other.getId());
+        }
+        return super.equals(obj);
+    }
+
 
 }
